fd9831456e2c29cd4421dccbd54002834edca109
KiraYagamov <isaru@sasuke.ru>
2023-11-03 11:20:18 +0300

    задачи егэ

--- DaryaProject/src/com/KiraYagamov/DaryaLessons/Lesson1HW.java
+++ DaryaProject/src/com/KiraYagamov/DaryaLessons/Lesson1HW.java
@@ -16,7 +16,8 @@ public class Lesson1HW {
             }
         }
     }
-    public String toBin(int num){
+    // для перевода в двочиную запись
+    public static String toBin(int num){
         String bin = "";
         while (num > 0){
             bin = (num % 2) + bin;
@@ -29,6 +30,63 @@ public class Lesson1HW {
         for(int i = 0; i < bin.length(); i++){
             if(bin.charAt(i) == '1'){
                 count++;
+            }
+        }
+        return count;
+    }
+    // изменение двоичного числа в зависимоти от количества в нем нулей и единиц
+    public static String editBin(String bin){
+        int oneCount = 0;
+        int zeroCount = 0;
+
+        for(int i = 0; i < bin.length(); i++){
+            char sim = bin.charAt(i);
+            if(sim == '0'){
+                zeroCount++;
+            }
+            else{
+                oneCount++;
+            }
+        }
+        if(oneCount == zeroCount){
+            bin += bin.charAt(bin.length()-1);
+        }
+        else if(oneCount > zeroCount){
+            bin += '0';
+        }
+        else{
+            bin += '1';
+        }
+        return bin;
+    }
+
+    // Для 14 задания
+    public static String toAnotherSystem(long num, int system){
+        String newNum = "";
+        while (num > 0){
+            newNum = (num % system) + newNum;
+            num = (long) num / system;
+        }
+        return newNum;
+    }
+    public static int zeroCount(String strNum){
+        int count = 0;
+        int notZeroIndex = -1;
+
+        // ищем первый символ не 0
+        for(int i = 0; i < strNum.length(); i++){
+            if(strNum.charAt(i) != '0'){
+                notZeroIndex = i;
+                break;
+            }
+        }
+
+        // считаем количество значащих нулей
+        if(notZeroIndex != -1) {
+            for (int i = notZeroIndex; i < strNum.length(); i++) {
+                if(strNum.charAt(i) == '0'){
+                    count++;
+                }
             }
         }
         return count;
